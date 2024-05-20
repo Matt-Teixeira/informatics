114abcc665498841d1c76712ea90f2e484892cfc
Matt Teixeira <matt-teixeira@vm-debian.c.oceanic-citadel-185620.internal>
2024-05-20 20:42:59 +0000

    set up table schemas for nullable fields

---
+++ sql/schemas/ge_ct.js
@@ -0,0 +1,12 @@
+const ge_ct_schema = {
+  system_id: null,
+  process_datetime: null,
+  capture_datetime: null,
+  host_datetime: null,
+  host_state: null,
+  prospective_exam: null,
+  protocol: null,
+  seven_up: null
+};
+
+module.exports = { ge_ct_schema };
