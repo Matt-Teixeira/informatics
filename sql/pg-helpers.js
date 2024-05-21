f64adba6cb58b4313f31060212a06d3078de2673
Matt Teixeira <matt-teixeira@vm-debian.c.oceanic-citadel-185620.internal>
2024-05-21 20:52:15 +0000

    Set up for bulk inserts

---
+++ sql/pg-helpers.js
@@ -0,0 +1,34 @@
+const pgp = require("pg-promise")();
+
+const pg_tables = {
+  ge: {
+    info_ge_ct: new pgp.helpers.TableName({
+      table: "ge_ct",
+      schema: "info"
+    })
+  }
+};
+
+const pg_column_sets = {
+  info: {
+    ge: {
+      ge_ct: new pgp.helpers.ColumnSet(
+        [
+          "system_id",
+          "process_datetime",
+          "capture_datetime",
+          "host_datetime",
+          "host_state",
+          "prospective_exam",
+          "protocol",
+          "serise",
+          "scans",
+          "images"
+        ],
+        { table: pg_tables.ge.info_ge_ct }
+      )
+    }
+  }
+};
+
+module.exports = { pg_tables, pg_column_sets };
