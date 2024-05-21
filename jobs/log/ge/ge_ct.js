3f1218eefaa6303a62e73a72871a3705712347ef
Matt Teixeira <matt-teixeira@vm-debian.c.oceanic-citadel-185620.internal>
2024-05-21 20:52:40 +0000

    Added parsers

--- jobs/log/ge/ge_ct.js
+++ jobs/log/ge/ge_ct.js
@@ -1,8 +1,10 @@
+const pgp = require("pg-promise")();
 const { get_ge_ct_data } = require("../../../sql/qf-provider");
 const map_to_schema = require("../../../tools/mapToSchema");
 
-const { parse_1 } = require("../../../processing/ge/ge_ct");
+const { parse_1, parse_2, parse_3 } = require("../../../processing/ge/ge_ct");
 const { ge_ct_schema } = require("../../../sql/schemas/ge_ct");
+const { pg_column_sets: pg_cs } = require("../../../sql/pg-helpers");
 
 // UTILS
 const db = require("../../../utils/db/pg-pool");
@@ -22,18 +24,28 @@ const { v4: uuidv4 } = require("uuid");
 const process_ge_ct = async (run_log, process_datetime) => {
   let note = {};
   const job_id = uuidv4();
-  await addLogEvent(I, run_log, "process_ge_ct", cal, null, null);
+  addLogEvent(I, run_log, "process_ge_ct", cal, null, null);
 
   const rows_to_process = await get_ge_ct_data(run_log);
 
   const parsed_data = [];
 
   for (let row of rows_to_process) {
-    parse_1(process_datetime, row, parsed_data);
+    // parse_1(process_datetime, row, parsed_data);
+    // parse_2(process_datetime, row, parsed_data);
+    parse_3(process_datetime, row, parsed_data);
   }
+  return;
 
   const mapped_data = map_to_schema(parsed_data, ge_ct_schema);
-  console.log(mapped_data);
+  c; // onsole.log(mapped_data);
+
+  const query = pgp.helpers.insert(mapped_data, pg_cs.info.ge.ge_ct);
+  try {
+    await db.any(query);
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 module.exports = process_ge_ct;
--- processing/ge/ge_ct.js
+++ processing/ge/ge_ct.js
@@ -16,4 +16,60 @@ const parse_1 = (process_datetime, row, parsed_data) => {
   }
 };
 
-module.exports = { parse_1 };
+const parse_2 = (process_datetime, row, parsed_data) => {
+  let db_obj = {};
+
+  const exam_end_re =
+    /Prospective Exam:\s?(?<prospective_exam>\d+).*Protocol:\s(?<protocol>\d+\.\d+)?/;
+  const series_block_re = /(?<series_block>Series:.*)/g;
+  const serise_data_re =
+    /(?<serise>\d+).*Scans:\s(?<scans>\d+).*\sImages:\s(?<images>\d+)/;
+
+  if (!row.end_msg) return;
+
+  const exam_end_matches = row.end_msg.match(exam_end_re);
+  const series_block_matches = row.message.match(series_block_re);
+
+  for (let serise of series_block_matches) {
+    if (exam_end_matches) {
+      db_obj.system_id = row.system_id;
+      db_obj.process_datetime = process_datetime;
+      db_obj.capture_datetime = row.capture_datetime;
+      db_obj.host_datetime = row.host_datetime;
+      db_obj.host_state = "end";
+      db_obj.prospective_exam = exam_end_matches.groups.prospective_exam;
+      db_obj.protocol = exam_end_matches.groups.protocol;
+    }
+    if (row.message) {
+      const serise_data = serise.match(serise_data_re);
+
+      if (serise_data) {
+        db_obj.serise = serise_data.groups.serise;
+        db_obj.scans = serise_data.groups.scans;
+        db_obj.images = serise_data.groups.images;
+      }
+    }
+    console.log(db_obj);
+    parsed_data.push(db_obj);
+  }
+};
+
+const parse_3 = (process_datetime, row, parsed_data) => {
+  const scan_meta_data_re = /Scan\sPrep:\sProspective\sExam:\s(?<prospective_exam>\d+).*Protocol:\s(?<protocol>\d+\.\d+)/;
+  const orientation_re = /Patient Orientation\((?<orientation>.*?)\).*Position\((?<position>.*)\)/;
+  const group_block_re = /(?<group_block>Group\(\d\).*)/g;
+
+  const group_block_data = row.message.match(group_block_re);
+
+  console.log("group_block_data");
+  console.log(group_block_data);
+};
+
+module.exports = { parse_1, parse_2, parse_3 };
+
+/* 
+[
+  'Group(1) ; Group Type(Scout) : Start Loc(200.000) : End Loc(-150.000)',
+  'Group(2) ; Group Type(Scout) : Start Loc(200.000) : End Loc(-150.000)'
+]
+*/
